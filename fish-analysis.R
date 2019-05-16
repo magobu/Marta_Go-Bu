2993fff999e9734c439b2605d96e7e540fe8a756
magobu <lapita@uw.edu>
2019-05-15 21:33:13 -0700

    Cange size cutoof for new column

--- fish-analysis.R
+++ fish-analysis.R
@@ -6,4 +6,7 @@ library(dplyr)
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
-#
+# Change category cut-oof-size
+
+fish_data_cat <- fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
